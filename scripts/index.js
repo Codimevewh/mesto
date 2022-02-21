1d8a582f9a9a7419906f04aefb772611bd5484bb
Dmitriy <codimevewh@yandex.ru>
2022-02-21 12:19:14 +0500

    Проектная работа 4 Исправления

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,32 @@
-// popup в DOM
 const popup = document.querySelector('.popup');
-
-// Botton сlose и edit в DOM
 const popupClose = document.querySelector('.popup__botton-close');
 const popupOpened = document.querySelector('.profile__botton-edit');
-
-// forms в DOM
-const formElement = document.querySelector('.popup__forms');
-
-// input в DOM
+const formElement = document.querySelector('.popup__form');
 const nameInput = formElement.querySelector('.popup__field_input_name');
 const jobInput = formElement.querySelector('.popup__field_input_job');
-
-// profile в DOM
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__job');
 
-// Пока все DOMа, Вкл/Выкл по click popup_opened
 
-function addOrRemovePopup() {
-    popup.classList.toggle('popup_opened')
+function addPopup() {
+    popup.classList.add('popup_opened')
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent;
 }
 
-popupClose.addEventListener('click', addOrRemovePopup);
-popupOpened.addEventListener('click', addOrRemovePopup);
+function removePopup() {
+    popup.classList.remove('popup_opened')
+}
 
-// SaveForm textContent по value
 function formSubmitHandler (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
-    addOrRemovePopup();
+    removePopup();
 }
 
-// Жми Botton-Save и да будет счастье
+popupOpened.addEventListener('click', addPopup);
+
+popupClose.addEventListener('click', removePopup);
+
 formElement.addEventListener('submit', formSubmitHandler);
